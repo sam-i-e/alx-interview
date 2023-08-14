30b2845537a1fbf5330911fc0592b36d24cd8e5c
sam-i-e <samierapo@outlook.com>
2023-08-14 22:54:52 +0300

    API fixes

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,16 +2,58 @@
 
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) throw err;
-  const actors = JSON.parse(body).characters;
-  exactOrder(actors, 0);
-});
-const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
+function getMovieCharacters (movieId) {
+  const filmUrl = `https://swapi.dev/api/films/${movieId}/`;
+  return new Promise((resolve, reject) => {
+    request.get(filmUrl, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        const filmData = JSON.parse(body);
+        resolve(filmData.characters);
+      }
+    });
   });
-};
+}
+
+function getCharacterName (characterUrl) {
+  return new Promise((resolve, reject) => {
+    request.get(characterUrl, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        const characterData = JSON.parse(body);
+        resolve(characterData.name);
+      }
+    });
+  });
+}
+
+function printCharacters (movieId) {
+  getMovieCharacters(movieId)
+    .then(characters => {
+      if (characters.length === 0) {
+        console.log('No characters found for the given movie ID.');
+        return;
+      }
+
+      Promise.all(characters.map(getCharacterName))
+        .then(characterNames => {
+          characterNames.forEach(name => console.log(name));
+        })
+        .catch(error => {
+          console.error('Error fetching character names:', error);
+        });
+    })
+    .catch(error => {
+      console.error('Error fetching movie characters:', error);
+    });
+}
+
+const movieId = process.argv[2];
+
+if (!movieId) {
+  console.log('Usage: ./script.js <movie_id>');
+} else {
+  printCharacters(movieId);
+}
